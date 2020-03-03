fe4d89fd124d6bb080adc455131e4a84e7593a7b
stevenhuang2020 <stevenhuang1017@gmail.com>
2020-03-03 21:40:41 +1300

    fucIntegral.py

--- fucIntegral.py
+++ fucIntegral.py
@@ -1,5 +1,7 @@
 #steven 02/03/2020
 #function integral to calculate area of curve and x axis
+import numpy as np 
+import matplotlib.pyplot as plt
 
 def fun(x):
     #return x 
@@ -19,8 +21,28 @@ def IntegralFuc():
     
     print(s)
 	
+def fillColor():
+    x = np.linspace(0,1.5, 100)
+    
+    y1 = np.zeros(len(x))
+    y2 = fun(x)
+    
+    #new_ticks = np.linspace(0,2,5) 
+    #print(new_ticks)
+    #plt.xticks(new_ticks)
+
+    plt.plot(x,y1,c='b',alpha=0.5)
+    plt.plot(x,y2,c='b',alpha=0.5)
+    
+
+    plt.fill_between(x,y1,y2,where=x<1,facecolor='green')
+    #plt.grid(True)
+    plt.show()
+
+
 def main():
    IntegralFuc()
+   fillColor()
 
 if __name__ == '__main__':
     main()
