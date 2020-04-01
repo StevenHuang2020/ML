3a0e6345fa779be0c207fba33a37c3267927a32b
StevenHuang2020 <61686583+StevenHuang2020@users.noreply.github.com>
2020-04-01 20:50:45 +1300

    Add files via upload

--- plotFunction.py
+++ plotFunction.py
@@ -6,7 +6,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 
+def derivative(f,x,h=0.0001): #one parameter fuction derivate
+    return (f(x+h)-f(x))/h
 
+def derivative2(f,x,h=0.001): #Second Derivative
+    return (derivative(f,x+h,h) - derivative(f,x,h))/h
+
+def normalDistribution(x):
+    return 1/np.sqrt(2*np.pi) * np.exp(-0.5*x**2)
+    
 def softmaxFuc(x):
     softmax = np.exp(x)/np.sum(np.exp(x))
     #print(softmax)
@@ -21,10 +29,6 @@ def log(x):
 
 def exp(x):
     return np.exp(x)
-
-def randomSymbol(length): #-1,1,1,-1,1,-1...
-    f = np.zeros(length)+np.where(np.random.random(length)>0.5,1.0,-1.0)
-    return f
     
 def circle(x):
     return np.sqrt(1-x**2)*randomSymbol(len(x))
@@ -32,6 +36,16 @@ def circle(x):
 def heart(x): #heart equation: x**2+ (5*y/4 - sqrt(abs(x)))**2 = 1
     a = np.sqrt(1-x**2)*randomSymbol(len(x)) + np.sqrt(abs(x))
     return a*4/5 
+
+def sawtoothWave(x): # y = t-floor(t) or y = t%1
+    #return x%1
+    #return x%1 + x
+    #return x%1 + np.sin(x)*x
+    a = 10 #perid
+    return 2*(x/a - np.floor(0.5+x/a))
+
+def powerX(x):
+    return np.power(x,x)
 
 def logisticMap2(r=1.5,x0=0.8,N=10): #x:= r*x(1-x)
     maps=[]
@@ -55,19 +69,24 @@ def logisticMap(r=1.5,x0=0.8,N=10): #x:= r*x(1-x)
 
     return maps
 
+def randomSymbol(length): #-1,1,1,-1,1,-1...
+    f = np.zeros(length)+np.where(np.random.random(length)>0.5,1.0,-1.0)
+    return f
+
 def plot(x,y=None):
     if y:
         plt.plot(x,y)
     else:
         plt.plot(x)
-    
     plt.show()
 
-def plotSub(x,y,ax=None,name=''):
-    ax.plot(x,y)
-    ax.title.set_text(name)
-    #ax.set_aspect(1)
-
+def plotSub(x,y,ax=None, aspect=False, label=''):
+    ax.plot(x,y,label=label)
+    #ax.title.set_text(name)
+    if aspect:
+        ax.set_aspect(1)
+    ax.legend()
+    
 def scatterSub(x,y,ax=None,name=''):
     ax.scatter(x,y,linewidths=.1,color='r',marker=',')
     ax.set_aspect(1)
@@ -104,6 +123,7 @@ def testLogisticMap2():
     #ls2 = logisticMap(r=rValue,x0=0.20000000001,N=100)
     #plot(ls2)
 
+    '''
     N=9
     for i in range(N):
         print('*'*50,i)
@@ -114,26 +134,46 @@ def testLogisticMap2():
         ax.plot(ls1,label='x0=0.2')
         ax.set_title('r='+str(rValue))
     plt.show()
+    '''
 
-def plotCircle():
+    iters=80
+    rStart = 3.9
+    #rl=[0, 0.01, 0.001, 0.0001, 0.0000001, 0.0000000001]
+    rl=[0, 0.000001, 0.0000000000001]
+    ls=[]
+    for i,rV in enumerate(rl):
+        #print('*'*50,i)
+        rValue = rStart + rV
+        ls.append(logisticMap(r=rValue,x0=0.8,N=iters))
+        
+    ax = plt.subplot(1,1,1)
+    for i,l in enumerate(ls):
+        ax.plot(l,label='x0='+str(rStart + rl[i]))
+
+    ax.set_title('Butterfly effects')
+    ax.legend(loc='lower left')
+    plt.show()
+
+
+def plotCircle(ax):
     x = np.linspace(-1.0, 1.0, 100)
     y = np.linspace(-1.0, 1.0, 100)
     X, Y = np.meshgrid(x, y)
     F = X ** 2 + Y ** 2 - 1
-    plt.contour(X, Y, F, [0])
-    plt.show()
-
-def plotHeart():
+    ax.contour(X, Y, F, [0])
+    ax.set_aspect(1)
+    
+def plotHeart(ax):
     x = np.linspace(-1.0, 1.0, 100)
     y = np.linspace(-1.0, 1.5, 100)
     X, Y = np.meshgrid(x, y)
     # x**2+ (5*y/4 - sqrt(abs(x)))**2 = 1
     F = X ** 2 + (5*Y/4 - np.sqrt(abs(x)))** 2 - 1
-    plt.contour(X, Y, F, [0])
-    plt.show()
-
+    ax.contour(X, Y, F, [0])
+    ax.set_aspect(1)
+    
 def main():
-    return testLogisticMap2()
+    #return testLogisticMap2()
 
     #x = np.linspace(-1,1, 10000)
     #scatter(x,circle(x))
@@ -142,24 +182,57 @@ def main():
     #scatter(np.arange(Num),logisticMap(r=.5,x0=0.4,N=Num),False)
     #plot(logisticMap(r=.5,x0=0.4,N=30))
 
-    #return plotCircle()
-    #return plotHeart()
-
-    ax = plt.subplot(2, 2, 1)
+    '''
+    row = 2
+    col = 2
+    ax = plt.subplot(row, col, 1)
     x = np.linspace(-5,5, 10)
     plotSub(x,sigmoid(x), ax,name='sigmod')
 
-    ax = plt.subplot(2, 2, 2)
+    ax = plt.subplot(row, col, 2)
     x = np.linspace(-5,0, 10)
     plotSub(x,exp(x), ax,name='exp')
 
-    ax = plt.subplot(2, 2, 3)
+    ax = plt.subplot(row, col, 3)
     x = np.linspace(-1,1, 1000)
     scatterSub(x,circle(x), ax,name='circle')
 
-    ax = plt.subplot(2, 2, 4)
+    ax = plt.subplot(row, col, 4)
     scatterSub(x,heart(x), ax,name='heart')
+    plt.show()
+    '''
 
+    '''
+    x = np.linspace(0,1.5, 100)
+    ax = plt.subplot(1,1,1)
+    #plotSub(x,sawtoothWave(x),ax,label='sawWave')
+    plotSub(x, powerX(x), ax,label='X^x')
+    plotSub(x, derivative(powerX, x), ax, label='X^x\'', aspect=False)
+    '''
+    
+    x = np.linspace(-2,2, 50)
+    ax = plt.subplot(1,1,1)
+
+    plotSub(x, normalDistribution(x), ax,label='normalDistribution')
+    plotSub(x, derivative(normalDistribution, x), ax, label='normalDistribution\'', aspect=False)
+    plotSub(x, derivative2(normalDistribution, x), ax, label='normalDistribution\'\'', aspect=False)
+    
+    plotSub(x, sigmoid(x), ax,label='sigmoid')
+    plotSub(x, derivative(sigmoid, x), ax, label='sigmoid\'', aspect=False)
+    plotSub(x, derivative2(sigmoid, x), ax, label='sigmoid\'\'', aspect=False)
+    
+    #plotSub(x, exp(x), ax,name='exp', label='exp')
+    #plotSub(x, derivative(exp, x), ax, label='exp\'', aspect=False)
+    
+    plotSub(x, softmaxFuc(x), ax,label='softmaxFuc')
+    plotSub(x, derivative(softmaxFuc, x), ax, label='softmaxFuc\'', aspect=False)
+    
+    #plotSub(x, log(x), ax,label='log')
+    #plotSub(x, derivative(log, x), ax,label='log\'')
+
+    
+    plotHeart(ax)
+    plotCircle(ax)
     plt.show()
 
 if __name__ == '__main__':
